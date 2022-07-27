06c799974c3b9f612eeb6288add7936320b9584c
Vladimir Marchenko <vladimir.marchenko@arm.com>
2022-07-27 10:22:43 +0200

    Updated documentation to Doxygen v1.9.2 and new layout. (#23)
    Update documentation to Doxygen v1.9.2 and new layout

--- DoxyGen/Doxygen_Templates/printComponentTabs.js
+++ DoxyGen/Doxygen_Templates/printComponentTabs.js
@@ -1,9 +1,9 @@
 var strgURL =   location.pathname;                      // path of current component
 
-// constructor for the array of objects
+// constuctor for the array of objects
 function tabElement(id, folderName, tabTxt )  {
-	this.id = id;                                       // elementID as needed in HTML; 
-	this.folderName = folderName;                       // folder name of the component 
+	this.id = id;                                       // elementID as needed in html;
+	this.folderName = folderName;                       // folder name of the component
     this.tabTxt = tabTxt;                               // Text displayed as menu on the web
 	this.currentListItem = '<li id="' + this.id + '" class="current"> <a href="../..' + this.folderName + 'index.html"><span>' + this.tabTxt + '</span></a></li>';
 	this.listItem = '<li id="' + this.id + '"> <a href="../..' + this.folderName + 'index.html"><span>' + this.tabTxt + '</span></a></li>';
@@ -13,16 +13,16 @@ function tabElement(id, folderName, tabTxt )  {
 var arr = [];
 
 // fill array
- arr.push( new tabElement( "GEN",     "/General/html/",     "General")   );
- 
+// arr.push( new    tabElement( "GEN",    "/General/html/", "General"));
+
 // write tabs
 // called from the header file.
 function writeComponentTabs()  {
   for ( var i=0; i < arr.length; i++ ) {
     if (strgURL.search(arr[i].folderName) > 0) {                    // if this is the current folder
       document.write(arr[i].currentListItem);                       // then print and highlight the tab
-    } else {                                                      
+    } else {
       document.write(arr[i].listItem);                              // else, print the tab
-    }                                                             
+    }
   }
 };

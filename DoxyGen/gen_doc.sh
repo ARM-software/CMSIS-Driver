cf634a3d94a1cdf31439af42fd7ccd375d41d2f1
Jonatan Antoni <jonatan.antoni@arm.com>
2022-08-09 12:21:40 +0200

    Update docs to latest structure

--- DoxyGen/gen_doc.sh
+++ DoxyGen/gen_doc.sh
@@ -1,78 +1,99 @@
 #!/bin/bash
 # Version: 1.0
-# Date: 2022-07-15
-# This bash script generates CSMSIS Driver documentation
+# Date: 2022-05-31
+# This bash script generates CMSIS-Driver Documentation:
 #
 # Pre-requisites:
 # - bash shell (for Windows: install git for Windows)
-# - doxygen 
-
+# - doxygen 1.9.2
 
 set -o pipefail
 
 DIRNAME=$(dirname $(readlink -f $0))
-DOXYGEN=$(which doxygen 2>/dev/null)
+DOXYGEN=$(which doxygen)
 REQ_DXY_VERSION="1.9.2"
-REGEN=0
-ALLPARTS=($(find ${DIRNAME} -mindepth 1 -maxdepth 1 -type d -exec basename {} \;))
-PARTS=()
+REQUIRED_GEN_PACK_LIB="0.1.0"
 
-if [[ -z "$*" ]]; then
-    REGEN=1
-else
-    for part in "$*"; do
-        if [[ " ${ALLPARTS[@]} " =~ " $part " ]]; then
-            PARTS+=($part)
-        fi
-    done
-fi
+############ gen-pack library ###########
 
-if [[ ! -f "${DOXYGEN}" ]]; then
-    echo "Doxygen not found!" >&2
-    echo "Did you miss to add it to PATH?"
-    exit 1
-else
-    version=$("${DOXYGEN}" --version | sed -E 's/.*([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
-    echo "Doxygen is ${DOXYGEN} at version ${version}"
-    if [[ "${version}" != "${REQ_DXY_VERSION}" ]]; then
-        echo "Doxygen required to be at version ${REQ_DXY_VERSION}!" >&2
-        exit 1
-    fi
-fi
-
-function doxygen {
-    partname=$(basename $(dirname $1))
-    if [[ $REGEN != 0 ]] || [[ " ${PARTS[@]} " =~ " ${partname} " ]]; then
-        pushd "$(dirname $1)" > /dev/null
-        echo "${DOXYGEN} $1"
-        "${DOXYGEN}" $(basename "$1")
-        popd > /dev/null
-        
-        if [[ $2 != 0 ]]; then
-            mkdir -p "${DIRNAME}/../Documentation/${partname}/html/search/"
-            cp -f "${DIRNAME}/Doxygen_Templates/search.css" "${DIRNAME}/../Documentation/${partname}/html/search/"
-        fi
-        
-        projectName=$(grep -E "PROJECT_NAME\s+=" $1 | sed -r -e 's/[^"]*"([^"]+)".*/\1/')
-        projectNumber=$(grep -E "PROJECT_NUMBER\s+=" $1 | sed -r -e 's/[^"]*"([^"]+)".*/\1/')
-        datetime=$(date -u +'%a %b %e %Y %H:%M:%S')
-        sed -e "s/{datetime}/${datetime}/" "${DIRNAME}/Doxygen_Templates/footer.js" \
-          | sed -e "s/{projectName}/${projectName}/" \
-          | sed -e "s/{projectNumber}/${projectNumber}/" \
-          > "${DIRNAME}/../Documentation/${partname}/html/footer.js"
-    fi
+function install_lib() {
+  local URL="https://github.com/Open-CMSIS-Pack/gen-pack/archive/refs/tags/v$1.tar.gz"
+  echo "Downloading gen_pack lib to '$2'"
+  mkdir -p "$2"
+  curl -L "${URL}" -s | tar -xzf - --strip-components 1 -C "$2" || exit 1
 }
 
-if [[ $REGEN != 0 ]]; then
-    if [[ -d "${DIRNAME}/../Documentation/" ]]; then
-        echo "Cleaning existing documentation ..."
-        find "${DIRNAME}/../Documentation/" -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} +
-    else
-        echo "Creating Documentation folder ..."
-        mkdir -p "${DIRNAME}/../Documentation/"
-    fi
+function load_lib() {
+  local GLOBAL_LIB="/usr/local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
+  local USER_LIB="${HOME}/.local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
+  if [[ ! -d "${GLOBAL_LIB}" && ! -d "${USER_LIB}" ]]; then
+    echo "Required gen-pack lib not found!" >&2
+    install_lib "${REQUIRED_GEN_PACK_LIB}" "${USER_LIB}"
+  fi 
+  
+  if [[ -d "${GLOBAL_LIB}" ]]; then
+    . "${GLOBAL_LIB}/gen-pack"
+  elif [[ -d "${USER_LIB}" ]]; then
+    . "${USER_LIB}/gen-pack"
+  else
+    echo "Required gen-pack lib is not installed!" >&2
+    exit 1
+  fi
+}
+
+load_lib
+find_git
+
+#########################################
+
+if [[ ! -f "${DOXYGEN}" ]]; then
+  echo "Doxygen not found!" >&2
+  echo "Did you miss to add it to PATH?"
+  exit 1
+else
+  version=$("${DOXYGEN}" --version | sed -E 's/.*([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+  echo "DOXYGEN is ${DOXYGEN} at version ${version}"
+  if [[ "${version}" != "${REQ_DXY_VERSION}" ]]; then
+    echo " >> Version is different from ${REQ_DXY_VERSION} !" >&2
+  fi
+fi
+
+if [ -z "$VERSION" ]; then
+  VERSION_FULL=$(git_describe)
+  VERSION=${VERSION_FULL%+*}
+else
+  VERSION_FULL=${VERSION}
 fi
 
 echo "Generating documentation ..."
-doxygen "${DIRNAME}/General/general.dxy" 1
+
+pushd $DIRNAME > /dev/null
+
+rm -rf ${DIRNAME}/../docs/html
+sed -e "s/{projectNumber}/${VERSION}/" "${DIRNAME}/driver.dxy.in" \
+  > "${DIRNAME}/driver.dxy"
+
+git_changelog -f html 1> history.txt 2>/dev/null
+
+echo "${DOXYGEN} driver.dxy"
+"${DOXYGEN}" driver.dxy
+popd > /dev/null
+
+if [[ $2 != 0 ]]; then
+  cp -f "${DIRNAME}/Doxygen_Templates/search.css" "${DIRNAME}/../docs/html/search/"
+fi
+
+projectName=$(grep -E "PROJECT_NAME\s+=" "${DIRNAME}/driver.dxy" | sed -r -e 's/[^"]*"([^"]+)"/\1/')
+datetime=$(date -u +'%a %b %e %Y %H:%M:%S')
+year=$(date -u +'%Y')
+if [[ "${year}" != "2022" ]]; then 
+  year="2022-${year}"
+fi
+sed -e "s/{datetime}/${datetime}/" "${DIRNAME}/Doxygen_Templates/footer.js.in" \
+  | sed -e "s/{year}/${year}/" \
+  | sed -e "s/{projectName}/${projectName}/" \
+  | sed -e "s/{projectNumber}/${VERSION}/" \
+  | sed -e "s/{projectNumberFull}/${VERSION_FULL}/" \
+  > "${DIRNAME}/../docs/html/footer.js"
+
 exit 0

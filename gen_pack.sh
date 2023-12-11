fef0d2aead5f4a378c170b0be58200fed7fb1556
Robert Rostohar <robert.rostohar@arm.com>
2023-12-11 14:08:46 +0100

    Update pack generation script

--- gen_pack.sh
+++ gen_pack.sh
@@ -1,24 +1,33 @@
-#!/bin/bash
-# Version: 2.2
-# Date: 2022-09-26
-# This bash script generates a CMSIS-Driver Software Pack
+#!/usr/bin/env bash
+# Version: 3.0
+# Date: 2023-11-06
+# This bash script generates a CMSIS Software Pack:
 #
 
 set -o pipefail
 
 # Set version of gen pack library
-REQUIRED_GEN_PACK_LIB="0.4.0"
+# For available versions see https://github.com/Open-CMSIS-Pack/gen-pack/tags.
+# Use the tag name without the prefix "v", e.g., 0.7.0
+REQUIRED_GEN_PACK_LIB="0.9.0"
 
 # Set default command line arguments
 DEFAULT_ARGS=(-c "")
 
 # Pack warehouse directory - destination
-PACK_OUTPUT=./output
+# Default: ./output
+#
+# PACK_OUTPUT=./output
 
-# Temporary pack build directory
-PACK_BUILD=./build
+# Temporary pack build directory,
+# Default: ./build
+#
+# PACK_BUILD=./build
 
 # Specify directory names to be added to pack base directory
+# An empty list defaults to all folders next to this script.
+# Default: empty (all folders)
+#
 PACK_DIRS="
   Config
   Documentation
@@ -32,61 +41,77 @@ PACK_DIRS="
 "
 
 # Specify file names to be added to pack base directory
+# Default: empty
+#
 PACK_BASE_FILES="
-  LICENSE.txt
-  README.md
+  LICENSE
 "
 
 # Specify file names to be deleted from pack build directory
+# Default: empty
+#
 PACK_DELETE_FILES=""
 
 # Specify patches to be applied
-PACK_PATCH_FILES=""
+# Default: empty
+#
+# PACK_PATCH_FILES=""
 
 # Specify addition argument to packchk
+# Default: empty
+#
 PACKCHK_ARGS=(-x M353 -x M364 -x M324 -x M382 -x M363 -x M362 -x M336)
 
-# custom preprocessing steps
+# Specify additional dependencies for packchk
+# Default: empty
+#
+# PACKCHK_DEPS=""
+
+# Optional: restrict fallback modes for changelog generation
+# Default: full
+# Values:
+# - full      Tag annotations, release descriptions, or commit messages (in order)
+# - release   Tag annotations, or release descriptions (in order)
+# - tag       Tag annotations only
+#
+PACK_CHANGELOG_MODE="tag"
+
+#
+# custom pre-processing steps
+#
+# usage: preprocess <build>
+#   <build>  The build folder
+#
 function preprocess() {
+  # add custom steps here to be executed
+  # before populating the pack build folder
   ./DoxyGen/gen_doc.sh
+  return 0
 }
 
+#
 # custom post-processing steps
-# function postprocess() {
-# }
+#
+# usage: postprocess <build>
+#   <build>  The build folder
+#
+function postprocess() {
+  # add custom steps here to be executed
+  # after populating the pack build folder
+  # but before archiving the pack into output folder
+  return 0
+}
 
 ############ DO NOT EDIT BELOW ###########
 
-function install_lib() {
-  local URL="https://github.com/Open-CMSIS-Pack/gen-pack/archive/refs/tags/v$1.tar.gz"
-  echo "Downloading gen-pack lib to '$2'"
-  mkdir -p "$2"
-  curl -L "${URL}" -s | tar -xzf - --strip-components 1 -C "$2" || exit 1
-}
+# Set GEN_PACK_LIB_PATH to use a specific gen-pack library root
+# ... instead of bootstrap based on REQUIRED_GEN_PACK_LIB
+if [[ -f "${GEN_PACK_LIB_PATH}/gen-pack" ]]; then
+  . "${GEN_PACK_LIB}/gen-pack"
+else
+  . <(curl -sL "https://raw.githubusercontent.com/Open-CMSIS-Pack/gen-pack/main/bootstrap")
+fi
 
-function load_lib() {
-  if [[ -d ${GEN_PACK_LIB} ]]; then
-    . "${GEN_PACK_LIB}/gen-pack"
-    return 0
-  fi
-  local GLOBAL_LIB="/usr/local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
-  local USER_LIB="${HOME}/.local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
-  if [[ ! -d "${GLOBAL_LIB}" && ! -d "${USER_LIB}" ]]; then
-    echo "Required gen_pack lib not found!" >&2
-    install_lib "${REQUIRED_GEN_PACK_LIB}" "${USER_LIB}"
-  fi
-
-  if [[ -d "${GLOBAL_LIB}" ]]; then
-    . "${GLOBAL_LIB}/gen-pack"
-  elif [[ -d "${USER_LIB}" ]]; then
-    . "${USER_LIB}/gen-pack"
-  else
-    echo "Required gen-pack lib is not installed!" >&2
-    exit 1
-  fi
-}
-
-load_lib
 gen_pack "${DEFAULT_ARGS[@]}" "$@"
 
 exit 0

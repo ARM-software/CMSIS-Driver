55860a7461c5aa92192e9b85aba1ae5d946bf735
Milorad Cvjetkovic <milorad.cvjetkovic@arm.com>
2024-05-28 07:17:23 +0200

    Update USB Host EHCI_TT and OHCI drivers config files and documentation (#31)

--- USB/EHCI/Config/USBH_EHCI_Config.h
+++ USB/EHCI/Config/USBH_EHCI_Config.h
@@ -1,6 +1,5 @@
-/* -----------------------------------------------------------------------------
- * Copyright (c) 2024 Arm Limited (or its affiliates).
- * All rights reserved.
+/*
+ * Copyright (c) 2024 Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -16,12 +15,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * -----------------------------------------------------------------------------
  *
- * $Date:        16. May 2024
- * $Revision:    V1.0
+ * $Date:       27. May 2024
+ * $Revision:   V1.0
  *
- * Project:      Config file for USB Host EHCI Controller Driver
- * -------------------------------------------------------------------------- */
+ * Project:     USB Host EHCI Controller Driver Configuration definitions
+ *
+ * -----------------------------------------------------------------------------
+ */
 
 //-------- <<< Use Configuration Wizard in Context Menu >>> --------------------
 
@@ -29,51 +31,54 @@
 #define USBH_EHCI_CONFIG_H_
 
 // <h>USB Host Controller 0
-//   <o>Driver number <0-3>
-//     <i>Specifies the exported Driver_USBH# instance
-//     <i>For example for value 0 the exported driver will be Driver_USBH0
+//   <o>Export control block Driver_USBH# <0-255>
+//     <i>Specifies the exported driver control block number
+//     <i>For example, for value 0 the exported driver control block will be Driver_USBH0
 #define USBH0_EHCI_DRV_NUM              0
 
-//   <o>EHCI registers base address <0-0xFFFFFFFF>
+//   <o>EHCI Registers base address <0-0xFFFFFFFF>
 //     <i>Specifies the absolute address at which EHCI controller registers are located
 #define USBH0_EHCI_BASE_ADDR            0x402E0100
 
-//   <e>Relocated EHCI communication area
+//   <e>Relocate EHCI Communication Area
 //     <i>Specifies if the communication area is located at a specific address
-//     <i>(via the linker script).
+//     <i>(via the linker script)
 #define USBH0_EHCI_COM_AREA_RELOC       0
 
-//     <s.64>EHCI communication area section name
+//     <s.64>Section name
 //       <i>Specifies the section name of the EHCI communication area
+//       <i>(for positioning via the linker script)
 #define USBH0_EHCI_COM_AREA_SECTION_NAME ".driver.usbh0.ehci_com_area"
 //   </e>
 // </h>
 
 // <e>USB Host Controller 1
-//   <i>Enables or disables USB Host Driver 1
+//   <i>Enables or disables USB Host Controller 1 Driver
 #define USBH1_EHCI_ENABLED              0
 
-//   <o>Driver number <0-3>
-//     <i>Specifies the exported Driver_USBH# instance
-//     <i>For example for value 1 the exported driver will be Driver_USBH1
+//   <o>Export control block Driver_USBH# <0-255>
+//     <i>Specifies the exported driver control block number
+//     <i>For example, for value 1 the exported driver control block will be Driver_USBH1
 #define USBH1_EHCI_DRV_NUM              1
 
-//   <o>EHCI Host Controller registers base address <0-0xFFFFFFFF>
+//   <o>EHCI Registers base address <0-0xFFFFFFFF>
 //     <i>Specifies the absolute address at which EHCI controller registers are located
 #define USBH1_EHCI_BASE_ADDR            0x402E0300
 
-//   <e>Relocated EHCI communication area
+//   <e>Relocate EHCI Communication Area
 //     <i>Specifies if the communication area is located at a specific address
-//     <i>(via the linker script).
+//     <i>(via the linker script)
 #define USBH1_EHCI_COM_AREA_RELOC       0
 
-//     <s.64>EHCI communication area section name
+//     <s.64>Section name
 //       <i>Specifies the section name of the EHCI communication area
+//       <i>(for positioning via the linker script)
 #define USBH1_EHCI_COM_AREA_SECTION_NAME ".driver.usbh1.ehci_com_area"
 //   </e>
 // </e>
 
-// <o>Maximum number of pipes (per controller) <1-15>
+// <o>Maximum number of Pipes <1-15>
+//   <i>Specifies the maximum number of pipes that the driver will support (per controller)
 #define USBH_EHCI_MAX_PIPES             4
 
 #endif /* USBH_EHCI_CONFIG_H_ */
--- USB/OHCI/Source/USBH_OHCI_Regs.h
+++ USB/OHCI/Source/USBH_OHCI_Regs.h
@@ -1,6 +1,5 @@
-/* -----------------------------------------------------------------------------
- * Copyright (c) 2024 Arm Limited (or its affiliates).
- * All rights reserved.
+/*
+ * Copyright (c) 2024 Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -16,12 +15,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * -----------------------------------------------------------------------------
  *
- * $Date:        15. May 2024
- * $Revision:    V1.0
+ * $Date:       28. May 2024
+ * $Revision:   V1.0
  *
- * Project:      USB Host OHCI Controller Driver Registers header
- * -------------------------------------------------------------------------- */
+ * Project:     USB Host OHCI Controller Driver Registers header
+ *
+ * -----------------------------------------------------------------------------
+ */
 
 #ifndef USBH_OHCI_REGS_H_
 #define USBH_OHCI_REGS_H_
